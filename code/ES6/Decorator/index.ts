a0a726f5ac124c6945657130e3fb50f6ce22e235
javaswing <zxdlovejava@gmail.com>
2023-08-24 19:03:06 +0800

    feat: add decorator code

--- code/ES6/Decorator/index.ts
+++ code/ES6/Decorator/index.ts
@@ -5,11 +5,21 @@ class Person {
     this.name = name;
   }
 
+  @loggedMethod
   greet() {
-    console.log('LOG: Entering method.');
-
     console.log(`Hello, my name is ${this.name}.`);
-
-    console.log('LOG: Exiting method.');
   }
 }
+
+function loggedMethod(originalMethod: any, _context: any) {
+  function replacementMethod(this: any, ...args: any[]) {
+    console.log('LOG: Entering method.');
+    const result = originalMethod.call(this, args);
+    console.log('LOG: Exiting method.');
+    return result;
+  }
+  return replacementMethod;
+}
+
+const p = new Person('Ron');
+p.greet();
