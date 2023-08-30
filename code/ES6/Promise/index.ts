19e882ab9c6d7cfbfd8bc999da693c2fd8c2558f
javaswing <zxdlovejava@gmail.com>
2023-08-30 19:34:07 +0800

    feat: add promise fn

--- code/ES6/Promise/index.ts
+++ code/ES6/Promise/index.ts
@@ -7,6 +7,8 @@ export default class CPromise {
   private status: Status;
   private value: any;
   private reason: any;
+  private onFulfilledFunArr: Function[] = [];
+  private onRejectedFunArr: Function[] = [];
 
   constructor(executor?: (resolve: Function, reject: Function) => void) {
     this.status = 'pending';
@@ -14,35 +16,138 @@ export default class CPromise {
     this.reason = undefined;
 
     const resolve = (value) => {
-      if (this.status === 'pending') {
-        this.value = value;
-        this.status = 'fulfilled';
+      if (value instanceof CPromise) {
+        value.then(resolve, reject);
       }
+
+      // 使用setTimeout来模拟异步任务，这里的使用setTimeout来保证
+      // 在状态变更的时候，再调用对应的方法
+      // 箭头函数保证this
+      setTimeout(() => {
+        if (this.status === 'pending') {
+          this.value = value;
+          this.status = 'fulfilled';
+
+          this.onFulfilledFunArr.forEach((fun) => fun(this.value));
+        }
+      });
     };
     const reject = (reason) => {
-      if (this.status === 'pending') {
-        this.reason = reason;
-        this.status = 'rejected';
-      }
+      setTimeout(() => {
+        if (this.status === 'pending') {
+          this.reason = reason;
+          this.status = 'rejected';
+          this.onRejectedFunArr.forEach((f) => f(this.reason));
+        }
+      });
     };
 
-    executor?.(resolve, reject);
+    try {
+      executor?.(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
   }
 
-  then(onFulfilled?: Function, onRejected?: Function) {
-    const fulfilled = onFulfilled ? onFulfilled : (value) => value;
-    const reject = onRejected
-      ? onRejected
-      : (error) => {
-          throw error;
-        };
+  then(onFulfilled?: Function | null, onRejected?: Function) {
+    let p1: CPromise;
+    // eslint-disable-next-line prefer-const
+    p1 = new CPromise((resolve, reject) => {
+      const onFulfilledFn = onFulfilled ? onFulfilled : (value) => value;
+      const onRejectFn = onRejected
+        ? onRejected
+        : (error) => {
+            throw error;
+          };
 
-    if (this.status === 'fulfilled') {
-      fulfilled(this.value);
+      if (this.status === 'fulfilled') {
+        try {
+          const r = onFulfilledFn(this.value);
+          this.resolutionProcedure(p1, r, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      }
+
+      if (this.status === 'rejected') {
+        try {
+          const r = onRejectFn(this.reason);
+          this.resolutionProcedure(p1, r, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      }
+
+      if (this.status === 'pending') {
+        this.onFulfilledFunArr.push(() => {
+          try {
+            const result = onFulfilledFn(this.value);
+            this.resolutionProcedure(p1, result, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+
+        this.onRejectedFunArr.push(() => {
+          try {
+            const r = onRejectFn(this.reason);
+            this.resolutionProcedure(p1, r, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+    });
+
+    return p1;
+  }
+
+  private resolutionProcedure(p: CPromise, result, resolve, reject) {
+    if (result === p) {
+      // 执行结果和当前promise对象一样，循环引用
+      return reject(new TypeError('Error'));
     }
 
-    if (this.status === 'rejected') {
-      reject(this.reason);
+    let isCalled = false;
+
+    // 如果result是一个新的promise
+    if (result !== null && (typeof result === 'object' || typeof result === 'function')) {
+      try {
+        if ('then' in result) {
+          const th = result.then;
+
+          if (typeof th === 'function') {
+            th.call(
+              result,
+              (y) => {
+                if (!isCalled) {
+                  isCalled = true;
+                  this.resolutionProcedure(p, y, resolve, reject);
+                }
+              },
+              function rj(r) {
+                if (!isCalled) {
+                  isCalled = true;
+                  reject(r);
+                }
+              }
+            );
+          }
+        } else {
+          resolve(result);
+        }
+      } catch (error) {
+        if (!isCalled) {
+          isCalled = true;
+          reject(error);
+        }
+      }
+    } else {
+      resolve(result);
     }
+  }
+
+  catch(onRejected) {
+    this.then(null, onRejected);
   }
 }
--- code/ES6/Promise/test/promise.test.ts
+++ code/ES6/Promise/test/promise.test.ts
@@ -2,11 +2,42 @@ import CPromise from '../index';
 
 // @link https://github.com/domenic/promise-tests/blob/master/lib/tests/promises-a.js
 describe('[Promise/A] Basic characteristics of `then`', () => {
-  test('test constructor', () => {
+  test('test base version', () => {
     new CPromise((resolve, _rejects) => {
       resolve(8888);
     }).then((res) => {
-      console.log('res', res);
+      expect(res).toEqual(8888);
     });
+  });
+
+  it('test then chain', () => {
+    new CPromise((resolve, _reject) => {
+      resolve(1);
+    })
+      .then((res) => {
+        return res + '2';
+      })
+      .then((res) => {
+        console.log('res', res);
+        expect(res).toEqual('12');
+      });
+  });
+
+  it('test then chain by new promise', () => {
+    expect.assertions(1);
+
+    new CPromise((resolve, _reject) => {
+      resolve(1);
+    })
+      .then((_res) => {
+        return new CPromise((resolve, _reject) => {
+          resolve(2);
+        });
+      })
+      .then((r) => {
+        console.log('r', r);
+        // expect(r).toBe(24);
+        return r;
+      });
   });
 });
