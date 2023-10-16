e198cb4c863adfcbb04ee2744b9604b988dc2f42
javaSwing <zxdlovejava@gmail.com>
2023-10-17 00:16:31 +0800

    feat: add webpack loader code

---
+++ code/webpack/loader/src/index.js
@@ -0,0 +1,6 @@
+function hello() {
+  console.log('hello world');
+  return 'hello';
+}
+
+export { hello };
---
+++ code/webpack/loader/webpack.config.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
